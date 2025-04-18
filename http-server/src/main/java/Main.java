691b869d4d697e429d06e0f7f08dfe39f6ac1394
DPRAKA15 <dpraka15@ford.com>
2025-04-18 21:26:01 +0530

    Basic Server Setup

---
+++ http-server/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Started HTTP-SERVER");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
+
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            System.out.println("Application Started Listening on port 4221");
+            Socket socket =  serverSocket.accept(); // Wait for connection from client.
+            System.out.println("accepted new connection");
+            InputStream inputStream = socket.getInputStream();
+            socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+}
